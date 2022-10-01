6d7fe260e6536ac0d2dc40538092eded8b15b012
tdomiris <113011309+tdomiris@users.noreply.github.com>
2022-10-02 00:01:00 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let insideOfBridge = document.getElementById("inside")
+
+insideOfBridge.addEventListener("click", () => {
+    document.getElementById("textbox").innerText = "Hi there. You clicked on the inside of the museum"
+})
+
+
